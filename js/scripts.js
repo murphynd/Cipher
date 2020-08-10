c92440abda2f0befe2e5fa684f4e82d98291cf4b
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-10 15:40:54 -0700

    it will add README and JS
    co-authored-by: Emme Buentiempo <emmettbuentiempo@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+//Start by prompting your user to enter a sentence. Then, store the sentence in a variable and print it to the console.
+var a = prompt("Write a sentance you would like to keep Top Secret")
+
+alert(a)
+//Next, create a function that will take the first and last letter of the sentence, capitalize them, and return this as a new string.
+function mixup ()
+//Create another function that will reverse the order of these two letters and return the result.
